7543b28182d688a725607130cdbf24c28c3d3c38
KaustubhPaliwal <122563165+KaustubhPaliwal@users.noreply.github.com>
2024-06-10 20:20:47 -0700

    All Voice Commands work, need to fix some collisions and record video

---
+++ Assets/Healing.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healing : MonoBehaviour
+{
+    public Canvas healingDone;
+    public Canvas transfStart;
+    public GameObject healing;
+    public Canvas page4;
+    public GameObject wrist;
+    public Material Highlight;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Contains("Vaccine"))
+        {
+            StartCoroutine(BodyScanRoutine());
+        }
+    }
+
+    private IEnumerator BodyScanRoutine()
+    {
+        healing.SetActive(true);
+        Destroy(this.GetComponent<SpriteRenderer>());
+        page4.gameObject.SetActive(false);
+        healingDone.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        healingDone.gameObject.SetActive(false);
+        transfStart.gameObject.SetActive(true);
+        wrist.GetComponent<MeshRenderer>().material = Highlight;
+    }
+}
